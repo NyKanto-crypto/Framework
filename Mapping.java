4ae724c619f9dad8f93a2eba8dfa937f691d0228
NyKanto-crypto <nykantorandri@gmail.com>
2023-06-09 22:00:37 +0300

    sprint4

--- Mapping.java
+++
@@ -1,6 +0,0 @@
-package etu1862.framework;
-
-public class Mapping {
-    String classname;
-    String method;
-}
--- Utils.java
+++
@@ -1,9 +0,0 @@
-package utils;
-
-public class Utils {
-    public String getspliturl(String url){
-        String[] url_split = url.split("/");
-
-        return url_split[url_split.length - 1];
-    }
-}
