61ccadeace1cab6f09b0a5f81c93df41e18b2663
NyKanto-crypto <nykantorandri@gmail.com>
2023-06-25 09:54:23 +0300

    sprint7

--- Framework/src/ModelView.java
+++ Framework/src/ModelView.java
@@ -1,7 +1,10 @@
 package view;
 
+import java.util.HashMap;
+
 public class ModelView {
     String view;
+    HashMap<String, Object> data = new HashMap<String, Object>();
 
     public String getView() {
         return view;
@@ -11,4 +14,15 @@ public class ModelView {
         this.view = view;
     }
 
+    public HashMap<String, Object> getData() {
+        return data;
+    }
+
+    public void setData(HashMap<String, Object> data) {
+        this.data = data;
+    }
+
+    public void addItem(String str, Object obj) {
+        this.data.put(str, obj);
+    }
 }
